9b349ab19d51d2d860e8f1b4d1ebbbec7be71aa3
Yuliya Dubovik <yuliya.dubovik@armtek.by>
2023-05-05 16:54:41 +0300

    client header

--- client/src/modules/page-layout/page-layout.module.ts
+++ client/src/modules/page-layout/page-layout.module.ts
@@ -4,9 +4,18 @@ import { AppMainComponent } from "./components/app-main/app-main-component";
 import { AppHeaderComponent } from "./components/app-header/app-header.component";
 import { AppFooterComponent } from "./components/app-footer/app-footer.component";
 import { PageLayoutRoutingModule } from "./page-layout-routing.module";
+import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
+import { MatBadgeModule } from "@angular/material/badge";
 
 @NgModule({
   declarations: [AppMainComponent, AppHeaderComponent, AppFooterComponent],
-  imports: [CommonModule, PageLayoutRoutingModule],
+  imports: [
+    CommonModule,
+    PageLayoutRoutingModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+  ],
 })
 export class PageLayoutModule {}
